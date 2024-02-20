6f4ab59cd495ea9b4afa07da30fd73df9a329f34
kamal9780 <kamal.dupleit@gmail.com>
2024-02-20 12:29:00 +0530

    fixed validation

--- src/utils/fieldValidations/auth.tsx
+++ src/utils/fieldValidations/auth.tsx
@@ -7,7 +7,16 @@ export const loginValidationSchema = Yup.object({
 
 /*** Change Passworsd */
 export const changePassValidationSchema = Yup.object({
-  newPassword: Yup.string().required("Password is required"),
+  newPassword: Yup.string().required("Password is required")
+    .matches(
+      /^(?=.*[A-Z])/, "Password must contain at least one uppercase letter"
+    )
+    .matches(
+      /^(?=.*[a-zA-Z].*[a-zA-Z].*[a-zA-Z].*[a-zA-Z])/, "Password must contain at least four alphabet letters"
+    )
+    .matches(
+      /^(?=.*\d.*\d.*\d.*\d)/, "Password must contain at least four numbers"
+    ),
   oldPassword: Yup.string().required("Password is required"),
   // confirmNewPassword: Yup.string().required("Password is required"),
   confirmPassword: Yup.string()
