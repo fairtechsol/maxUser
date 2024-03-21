309c406212c9f6357e99b780d902c3c84d22b46b
kamal9780 <kamal.dupleit@gmail.com>
2024-03-21 16:42:59 +0530

    updated P/L table

--- src/components/profitLoss/index.tsx
+++ src/components/profitLoss/index.tsx
@@ -52,7 +52,7 @@ const ProfitLossComponent = () => {
 
   return (
     <>
-      <div className="whitespace">
+    {isMobile && <div>
         <ReportContainer title="Profit Loss">
           <Stack gap={2}>
             <Row className="g-2 mt-1">
@@ -122,7 +122,78 @@ const ProfitLossComponent = () => {
             </CustomTable>
           </Stack>
         </ReportContainer>
-      </div>
+      </div>}
+     {!isMobile && <div className="whitespace">
+        <ReportContainer title="Profit Loss">
+          <Stack gap={2}>
+            <Row className="g-2 mt-1">
+              <Col md={2} xs={6}>
+                <DatePicker
+                  onChange={setFromDate}
+                  value={fromDate}
+                  closeCalendar={true}
+                  clearIcon={false}
+                  className="w-100"
+                />
+                {/* <CustomInput type="date" /> */}
+              </Col>
+              <Col md={2} xs={6}>
+                <DatePicker
+                  onChange={setToDate}
+                  value={toDate}
+                  closeCalendar={true}
+                  clearIcon={false}
+                  className="w-100"
+                />
+                {/* <CustomInput type="date" /> */}
+              </Col>
+
+              <Col md={2} xs={12}>
+                <CustomButton
+                  onClick={handleSubmit}
+                  size={isMobile ? "sm" : "lg"}
+                  className={`${
+                    isMobile ? "w-100" : " bg-primaryBlue"
+                  } border-0 `}
+                >
+                  Submit
+                </CustomButton>
+              </Col>
+            </Row>
+            <CustomTable
+              bordered={true}
+              striped={!isMobile}
+              columns={[
+                {
+                  id: "eventType",
+                  label: "Event Type",
+                },
+                {
+                  id: "eventName",
+                  label: "Event Name",
+                },
+                {
+                  id: "amount",
+                  label: "Amount",
+                },
+              ]}
+              itemCount={10}
+              setTableConfig={() => {}}
+            >
+              {profitLossReport &&
+                profitLossReport?.result?.map((item: any, index: number) => {
+                  return (
+                    <tr className={`${isMobile && "title-12"}`} key={index}>
+                      <td>{item?.eventType}</td>
+                      <td>{item?.marketType}</td>
+                      <td>{item?.aggregateAmount}</td>
+                    </tr>
+                  );
+                })}
+            </CustomTable>
+          </Stack>
+        </ReportContainer>
+      </div>}
     </>
   );
 };
