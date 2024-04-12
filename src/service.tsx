f14b26d4caa36b7eab3929cf7199a514d0d01826
PardeepDuple <pradeep.duple@gmail.com>
2024-04-12 13:49:35 +0530

    commented notification for 401 error

--- src/service.tsx
+++ src/service.tsx
@@ -61,7 +61,7 @@ service.interceptors.response.use(
     } else if (error.response.status === 409) {
       toast.error(error.response.data.message);
     } else if (error.response.status === 401) {
-      toast.error(error.response.data.message);
+      // toast.error(error.response.data.message);
       window.location.replace("/login");
       sessionStorage.clear();
     }
