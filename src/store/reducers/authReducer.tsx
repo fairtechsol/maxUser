473fdcd06bb400a73fc5d8e97ee73386bb18478f
PardeepDuple <pradeep.duple@gmail.com>
2024-04-18 16:45:31 +0530

    initially set to false

--- src/store/reducers/authReducer.tsx
+++ src/store/reducers/authReducer.tsx
@@ -30,6 +30,7 @@ export const authReducer = createReducer(initialState, (builder) => {
     })
     .addCase(checkOldPassword.pending, (state) => {
       state.loading = true;
+      state.oldPasswordMatched = false;
     })
     .addCase(checkOldPassword.fulfilled, (state, action) => {
       state.loading = false;
