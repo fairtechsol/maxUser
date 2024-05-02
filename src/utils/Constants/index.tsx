f993240b7cffdfcfcec366c9be51c0caba35add3
kamal9780 <kamal.dupleit@gmail.com>
2024-05-02 15:19:03 +0530

    updated

--- src/utils/Constants/index.tsx
+++ src/utils/Constants/index.tsx
@@ -44,7 +44,7 @@ export const Constants = {
   apiBasePathLive: "https://betfairapi.fairgame7.com",
   thirdPartyLive: "https://serviceapi.fairgame7.com",
   expertPathLive: "https://expertapi.fairgame7.com",
-  localPath: "http://localhost:5001",
+  localPath: "http://localhost:5000",
   localPathExpert: "http://localhost:6060",
 };
 
