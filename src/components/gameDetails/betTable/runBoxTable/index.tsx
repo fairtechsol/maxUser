12dea3545af38bce9fea00a849bb619cf0bcf361
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-05-03 16:24:30 +0530

    run box component ui

--- src/components/gameDetails/betTable/runBoxTable/index.tsx
+++ src/components/gameDetails/betTable/runBoxTable/index.tsx
@@ -2,26 +2,27 @@ import { Table } from "react-bootstrap";
 
 const RunBoxTable = ({ runAmount }: any) => {
   return (
-    <Table striped bordered hover>
+    <Table striped bordered hover style={{width:'90%', margin:'17px'}}>
       <thead>
-        <tr>
-          <th>Run </th>
-          <th className="text-center">Amount</th>
+        <tr >
+          <th style={{backgroundColor: '#ffc742',textAlign:'center'}}>Run </th>
+          <th className="text-end" style={{backgroundColor: '#ffc742'}}>Amount</th>
         </tr>
       </thead>
-      <tbody className="text-center">
+      <tbody>
         {runAmount?.betPlaced?.map((item: any, index: number) => {
           return (
             <tr key={index}>
-              <td className={+item?.profitLoss >= 0 ? "bg-blue1" : "bg-red1"}>
+              <td className={+item?.profitLoss >= 0 ? "bg-blue1" : "bg-red1"} style={{textAlign:'center'}}>
                 {item?.odds}
               </td>
-              <td className={+item?.profitLoss >= 0 ? "bg-blue1" : "bg-red1"}>
-                {item?.profitLoss}
+              <td className={+item?.profitLoss >= 0 ? "bg-blue1" : "bg-red1"} style={{textAlign:'end'}}>
+                {parseFloat(item?.profitLoss).toFixed(2)}
               </td>
             </tr>
           );
         })}
+        
       </tbody>
     </Table>
   );
