59a5013210747ca6a18bc0e97dfcbbca495a7572
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-05-09 11:48:54 +0530

    football tennis manual bookmaker decimal

--- src/components/otherGameDetaills/betTable/bookMaker/index.tsx
+++ src/components/otherGameDetaills/betTable/bookMaker/index.tsx
@@ -262,6 +262,9 @@ function FootballBookmakerTable({
                       ?.map((_: any, index: number) => (
                         <BackLayBox
                           key={index}
+                          indexs={index}
+                          type={data.type}
+                          box={'back'}
                           customClass={`match-odd-bet-place ${
                             isMobile && backLayCount != 2
                               ? "bookmaker-width-26"
@@ -314,6 +317,9 @@ function FootballBookmakerTable({
                       ?.map((_: any, index: number) => (
                         <BackLayBox
                           key={index}
+                          indexs={index}
+                          type={data.type}
+                          box={'lay'}
                           customClass={`match-odd-bet-place ${
                             isMobile && backLayCount != 2
                               ? "bookmaker-width-26"
