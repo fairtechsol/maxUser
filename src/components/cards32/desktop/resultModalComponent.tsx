c5a611679171d94024bcf1f5ca409793e952ccdb
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-06-21 16:52:48 +0530

    card32 result modal ui with integration

--- src/components/cards32/desktop/resultModalComponent.tsx
+++ src/components/cards32/desktop/resultModalComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { HandleCards } from "../../commonComponent/cardsComponent";
 import { FaTrophy } from "react-icons/fa";
 interface Props {
@@ -10,15 +10,20 @@ interface Props {
 }
 
 const Card32ResultComponent: React.FC<Props> = ({ data }: any) => {
-console.log("data",data)
-const resultCards = data?.result?.cards?.split(',')
-const desc = data?.result?.desc?.split('*')
-const resultData = desc?.[0]?.split('|')
-const dragonData = desc?.[1]?.split('|')
-const tigerData = desc?.[2]?.split('|')
-const dragonCard = dragonData?.[2]
-const tigerCard  = tigerData?.[2]
-
+  const resultCards = data?.result?.cards?.split(",");
+  let result: string[][] = [[], [], [], []];
+  if (resultCards) {
+    resultCards?.forEach((item: any, index: any) => {
+      const targetArray = index % 4;
+      result[targetArray].push(item);
+    });
+  }
+console.log('first',result)
+  const allKeys = Object.keys(data ? data : 0);
+  const cArray = allKeys?.filter((key) => /^C\d+$/.test(key));
+  const numbers = cArray.map((key) => Number(data[key]));
+  const max = Math.max(...numbers);
+  console.log("data", numbers);
   return (
     <Container style={{ display: "flex", flexDirection: "column" }}>
       <div className="card32resultModal">
@@ -30,76 +35,92 @@ const tigerCard  = tigerData?.[2]
                 <FaTrophy size={30} color="#169733" />
               </div>
             )}
-            <div
-              style={{
-                border: "1px solid #fdef34",
-                borderRadius: "1px",
-                marginLeft: "5px",
-              }}
-            >
-              <HandleCards card={resultCards?.[0]} />
-            </div>
+            {result?.[0]?.map((item: any) => {
+              return item != "1" && (
+                <div
+                  style={{
+                    border: "1px solid #fdef34",
+                    borderRadius: "1px",
+                    marginLeft: "5px",
+                  }}
+                >
+                  <HandleCards card={item} />
+                </div>
+              );
+            })}
+           
           </div>
         </div>
         <div className="card32resultCardContainer">
           <span className="fs-5">Player 9</span>
           <div className="d-sm-flex flex-row justify-content-center align-items-center">
-              {data?.result?.win === "2" && (
-            <div className="casino-winner-icon">
-              <FaTrophy size={30} color="#169733" />
-            </div>
-              )}
-            <div
-              style={{
-                border: "1px solid #fdef34",
-                borderRadius: "1px",
-                marginLeft: "5px",
-              }}
-            >
-              <HandleCards card={resultCards?.[1]} />
-            </div>
+            {data?.result?.win === "2" && (
+              <div className="casino-winner-icon">
+                <FaTrophy size={30} color="#169733" />
+              </div>
+            )}
+           {result?.[1]?.map((item: any) => {
+              return item != "1" && (
+                <div
+                  style={{
+                    border: "1px solid #fdef34",
+                    borderRadius: "1px",
+                    marginLeft: "5px",
+                  }}
+                >
+                  <HandleCards card={item} />
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="card32resultCardContainer">
           <span className="fs-5">Player 10</span>
           <div className="d-sm-flex flex-row justify-content-center align-items-center">
-              {data?.result?.win === "2" && (
-            <div className="casino-winner-icon">
-              <FaTrophy size={30} color="#169733" />
-            </div>
-              )}
-            <div
-              style={{
-                border: "1px solid #fdef34",
-                borderRadius: "1px",
-                marginLeft: "5px",
-              }}
-            >
-              <HandleCards card={resultCards?.[1]} />
-            </div>
+            {data?.result?.win === "3" && (
+              <div className="casino-winner-icon">
+                <FaTrophy size={30} color="#169733" />
+              </div>
+            )}
+            {result?.[2]?.map((item: any) => {
+              return item != "1" && (
+                <div
+                  style={{
+                    border: "1px solid #fdef34",
+                    borderRadius: "1px",
+                    marginLeft: "5px",
+                  }}
+                >
+                  <HandleCards card={item} />
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="card32resultCardContainer">
           <span className="fs-5">Player 11</span>
           <div className="d-sm-flex flex-row justify-content-center align-items-center">
-              {data?.result?.win === "2" && (
-            <div className="casino-winner-icon">
-              <FaTrophy size={30} color="#169733" />
-            </div>
-              )}
-            <div
-              style={{
-                border: "1px solid #fdef34",
-                borderRadius: "1px",
-                marginLeft: "5px",
-              }}
-            >
-              <HandleCards card={resultCards?.[1]} />
-            </div>
+            {data?.result?.win === "4" && (
+              <div className="casino-winner-icon">
+                <FaTrophy size={30} color="#169733" />
+              </div>
+            )}
+            {result?.[3]?.map((item: any) => {
+              return item != "1" && (
+                <div
+                  style={{
+                    border: "1px solid #fdef34",
+                    borderRadius: "1px",
+                    marginLeft: "5px",
+                  }}
+                >
+                  <HandleCards card={item} />
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
-     
     </Container>
   );
 };
