26101565fc141a11b939c4d07f7706aa5a046113
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-07-04 15:43:08 +0530

    open teenpatti

--- src/utils/Constants/index.tsx
+++ src/utils/Constants/index.tsx
@@ -1430,7 +1430,7 @@ export const casinoIcons = [
     name: "Test Teenpatti",
   },
   {
-    url: "",
+    url: "/teen8",
     imgSrc:
       "https://dzm0kbaskt4pv.cloudfront.net/v12/static/front/img/casinoicons/img/teenpatti.jpg",
     name: "Open Teenpatti",
@@ -1578,7 +1578,7 @@ export const card3 = {
       name: "1 DAY TEENPATTI",
     },
     {
-      url: "/teen9",
+      url: "",
       imgSrc: testteen,
       name: "TEST TEENPATTI",
     },
