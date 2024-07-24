44434dd6414383b4594f293a721a15009ca9e4a4
AkashDuple <160487146+AkashDuple@users.noreply.github.com>
2024-07-24 12:53:46 +0530

    3cardj

--- src/components/3CardJ/desktop/CommonCardImg/index.tsx
+++ src/components/3CardJ/desktop/CommonCardImg/index.tsx
@@ -37,7 +37,7 @@ const CommonCardImg = ({
     }
   }, [data?.no?.gstatus]);
 
-  console.log("lock", data);
+  
   useEffect(() => {
     handleBet();
   }, [nat]);
--- src/components/3CardJ/desktop/index.tsx
+++ src/components/3CardJ/desktop/index.tsx
@@ -73,7 +73,7 @@ const CardJDesktop = () => {
     };
   }, [lastActivityTime, showInactivityModal]);
 
-  console.log("detail",dragonTigerDetail)
+  
   return (
     <>
       <Row>
