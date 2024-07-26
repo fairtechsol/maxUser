3ac1b9be0b57d9b6a9fbe91b68645f4f7306dcff
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-07-26 12:00:10 +0530

    worli

--- src/components/worli/desktop/CardsBox/index.tsx
+++ src/components/worli/desktop/CardsBox/index.tsx
@@ -1,31 +1,31 @@
-// import { useDispatch } from "react-redux";
-// import { AppDispatch } from "../../../../store/store";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../../store/store";
 // import CommonCardImg from "../CommonCardImg";
-// import { selectedBetAction } from "../../../../store/actions/match/matchListAction";
+import { selectedBetAction } from "../../../../store/actions/match/matchListAction";
 import "./style.scss";
 
 const CardBox = ({ title, data, cards, odds }: any) => {
   const dispatch: AppDispatch = useDispatch();
 
-  // const handleBet = (item: any) => {
-  //   let team = {
-  //     bettingType: "BACK",
-  //     matchId: data?.id,
-  //     odd: item?.rate,
-  //     stake: 0,
-  //     matchBetType: "matchOdd",
-  //     betOnTeam: item?.nat,
-  //     name: item?.nat,
-  //     bettingName: "Match odds",
-  //     selectionId: item?.sid,
-  //   };
-  //   dispatch(
-  //     selectedBetAction({
-  //       team,
-  //       data,
-  //     })
-  //   );
-  // };
+  const handleBet = (item: any) => {
+    let team = {
+      bettingType: "BACK",
+      matchId: data?.id,
+      odd: item?.rate,
+      stake: 0,
+      matchBetType: "matchOdd",
+      betOnTeam: item?.nat,
+      name: item?.nat,
+      bettingName: "Match odds",
+      selectionId: item?.sid,
+    };
+    dispatch(
+      selectedBetAction({
+        team,
+        data,
+      })
+    );
+  };
   // const arCards = cards?.ar?.split(",");
   // const brCards = cards?.br?.split(",");
   return (
