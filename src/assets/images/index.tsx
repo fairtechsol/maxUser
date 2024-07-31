fcb8e9919bc7d1676f2cd3f2c6061e11d17260cd
kamal <154864041+kamal9780@users.noreply.github.com>
2024-07-31 13:44:10 +0530

    updated rules modal for super over

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -83,3 +83,4 @@ export { default as bac1 } from "./baccarat/baccarat1.webp";
 export { default as bac2 } from "./baccarat/baccarat2.webp";
 export { default as loadernew } from "./loader.gif";
 export { default as warrules } from "./casinowar/war.jpg";
+export { default as supoerrules } from "./superOver/superoverRules.webp";
