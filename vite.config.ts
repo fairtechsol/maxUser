5ec0ec69f14268db6f1d062d7bf253285579d308
PardeepDuple <pradeep.duple@gmail.com>
2024-08-02 12:26:00 +0530

    update vite and other dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      // ...other options
-      output: {
-        assetFileNames: "[name].[ext]", // Preserve original file names
-      },
-    },
-  },
+  // build: {
+  //   sourcemap: false,
+  //   rollupOptions: {
+  //     // ...other options
+  //     output: {
+  //       assetFileNames: "[name].[ext]", // Preserve original file names
+  //     },
+  //   },
+  // },
 });
