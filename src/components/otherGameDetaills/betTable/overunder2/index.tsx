e382e193541b8cb6e5a0fe3a06dd6461a4014296
PardeepDuple <pradeep.duple@gmail.com>
2024-08-05 13:49:40 +0530

    logic for place bet profit loss and rates show

--- src/components/otherGameDetaills/betTable/overunder2/index.tsx
+++ src/components/otherGameDetaills/betTable/overunder2/index.tsx
@@ -1,18 +1,84 @@
 import React from "react";
 import "../style.scss";
 import isMobile from "../../../../utils/screenDimension";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../../store/store";
+import { selectedBetAction } from "../../../../store/actions/match/matchListAction";
+import {
+  profitLossDataForMatchConstants,
+  teamStatus,
+} from "../../../../utils/constants";
 
-const MarketRow = ({ title, odds }: any) => {
-  // console.log(odds, "dfsgdf")
+const MarketRow = ({ title, odds, data, matchDetails, team }: any) => {
+  const dispatch: AppDispatch = useDispatch();
+  const handlePlaceBet = (team: any, data: any) => {
+    dispatch(
+      selectedBetAction({
+        team,
+        data,
+      })
+    );
+  };
   return (
     <div className="market-row-o" data-title="ACTIVE">
       <div className="market-nation-detail-o">
         <span className="market-nation-name-o ">{title}</span>
-        <div className="market-nation-book-o"></div>
+        <div
+          className={`market-nation-book-o ${
+            team === "teamA"
+              ? matchDetails?.profitLossDataMatch[
+                  profitLossDataForMatchConstants[data?.type]?.A
+                ] < 0
+                ? "color-red"
+                : "color-green"
+              : matchDetails?.profitLossDataMatch[
+                  profitLossDataForMatchConstants[data?.type]?.B
+                ] < 0
+              ? "color-red"
+              : "color-green"
+          }`}
+        >
+          {team === "teamA"
+            ? matchDetails?.profitLossDataMatch[
+                profitLossDataForMatchConstants[data?.type]?.A
+              ] ?? 0
+            : matchDetails?.profitLossDataMatch[
+                profitLossDataForMatchConstants[data?.type]?.B
+              ] ?? 0}
+        </div>
       </div>
-      {odds?.map((odd: any, index: any) => (
-        <div key={index} className={`market-odd-box-o ${odd.className}`}>
-          <span className="market-odd-o">{odd.value}</span>
+      {odds?.map((odd: any) => (
+        <div
+          key={odd?.id}
+          className={`market-odd-box-o ${odd.className}`}
+          onClick={() => {
+            if (
+              odd.value > 0 &&
+              data?.runners?.[
+                team === "teamA" ? 0 : 1
+              ]?.status?.toLowerCase() == teamStatus.active?.toLowerCase()
+            ) {
+              handlePlaceBet(
+                {
+                  betOnTeam: team === "teamA" ? "UNDER" : "OVER",
+                  rate: odd.value,
+                  type: odd.type,
+                  stake: 0,
+                  teamA: "UNDER",
+                  teamB: "OVER",
+                  betId: data?.id,
+                  eventType: matchDetails?.matchType,
+                  matchId: matchDetails?.id,
+                  placeIndex: odd.pI,
+                  matchBetType: data?.type,
+                  gameType: "other",
+                },
+                data
+              );
+            }
+          }}
+        >
+          <span className="market-odd-o">{odd.value ?? "-"}</span>
           <span className="market-volume-o">{odd.volume}</span>
         </div>
       ))}
@@ -20,72 +86,143 @@ const MarketRow = ({ title, odds }: any) => {
   );
 };
 
-const OverUnderMarket2 = ({
-  minMax,
-  data,
-  backLayCount,
-  matchDetails,
-  title,
-}: any) => {
-  console.log(data, "hbdj");
+const OverUnderMarket2 = ({ minMax, data, matchDetails }: any) => {
   return (
-    <div className={`game-market-o market-4-o`}>
+    <div className="game-market-o market-4-o">
       <div className="market-header-o d-flex">
         <div className="market-nation-detail-o">
           <span className="market-nation-name-o">
             Max: {minMax.max} / Min: {minMax.min}
           </span>
         </div>
-         {!isMobile && (
+        {!isMobile && (
           <>
-            <div className={`market-odd-box-o`}></div>
-            <div className={`market-odd-box-o`}></div>
-          </>)
-        }
-        <div className={`market-odd-box-o back`}>{<b>{"BACK"}</b>}</div>
-        <div className={`market-odd-box-o lay`}>{<b>{"LAY"}</b>}</div>
-        <div className={`market-odd-box-o`}></div>
-        <div className={`market-odd-box-o`}></div>
+            <div className="market-odd-box-o"></div>
+            <div className="market-odd-box-o"></div>
+          </>
+        )}
+        <div className="market-odd-box-o back">
+          <b>BACK</b>
+        </div>
+        <div className="market-odd-box-o lay">
+          <b>LAY</b>
+        </div>
+        <div className="market-odd-box-o"></div>
+        <div className="market-odd-box-o"></div>
       </div>
       <div className="market-body-o" data-title="OPEN">
-        {/* {data?.map((market:any, index:any) => ( */}
         <MarketRow
-          //   key={index}
-          title={data?.name}
+          title="Under"
+          data={data}
+          matchDetails={matchDetails}
+          team="teamA"
           odds={[
-            { className: "back2", value: data.backTeamA, volume: "N/A" },
-            { className: "back1", value: data.backTeamB, volume: "N/A" },
-            { className: "back", value: data.backTeamC, volume: "N/A" },
-            { className: "lay", value: data.layTeamA, volume: "N/A" },
-            { className: "lay1", value: data.layTeamB, volume: "N/A" },
-            { className: "lay2", value: data.layTeamC, volume: "N/A" },
+            {
+              id: 1,
+              className: "back2",
+              value: data?.runners?.[0]?.ex?.availableToBack?.[2]?.price,
+              volume: data?.runners?.[0]?.ex?.availableToBack?.[2]?.size,
+              pI: 2,
+              type: "back",
+            },
+            {
+              id: 2,
+              className: "back1",
+              value: data?.runners?.[0]?.ex?.availableToBack?.[1]?.price,
+              volume: data?.runners?.[0]?.ex?.availableToBack?.[1]?.size,
+              pI: 1,
+              type: "back",
+            },
+            {
+              id: 3,
+              className: "back",
+              value: data?.runners?.[0]?.ex?.availableToBack?.[0]?.price,
+              volume: data?.runners?.[0]?.ex?.availableToBack?.[0]?.size,
+              pI: 0,
+              type: "back",
+            },
+            {
+              id: 4,
+              className: "lay",
+              value: data?.runners?.[0]?.ex?.availableToLay?.[0]?.price,
+              volume: data?.runners?.[0]?.ex?.availableToLay?.[0]?.size,
+              pI: 0,
+              type: "lay",
+            },
+            {
+              id: 5,
+              className: "lay1",
+              value: data?.runners?.[0]?.ex?.availableToLay?.[1]?.price,
+              volume: data?.runners?.[0]?.ex?.availableToLay?.[1]?.size,
+              pI: 1,
+              type: "lay",
+            },
+            {
+              id: 6,
+              className: "lay2",
+              value: data?.runners?.[0]?.ex?.availableToLay?.[2]?.price,
+              volume: data?.runners?.[0]?.ex?.availableToLay?.[2]?.size,
+              pI: 2,
+              type: "lay",
+            },
           ]}
         />
         <MarketRow
-          //   key={index}
-          title={data?.name}
+          title="Over"
+          data={data}
+          matchDetails={matchDetails}
+          team="teamB"
           odds={[
-            { className: "back2", value: data.backTeamA, volume: "N/A" },
-            { className: "back1", value: data.backTeamB, volume: "N/A" },
-            { className: "back", value: data.backTeamC, volume: "N/A" },
-            { className: "lay", value: data.layTeamA, volume: "N/A" },
-            { className: "lay1", value: data.layTeamB, volume: "N/A" },
-            { className: "lay2", value: data.layTeamC, volume: "N/A" },
+            {
+              id: 1,
+              className: "back2",
+              value: data?.runners?.[1]?.ex?.availableToBack?.[2]?.price,
+              volume: data?.runners?.[1]?.ex?.availableToBack?.[2]?.size,
+              pI: 2,
+              type: "back",
+            },
+            {
+              id: 2,
+              className: "back1",
+              value: data?.runners?.[1]?.ex?.availableToBack?.[1]?.price,
+              volume: data?.runners?.[1]?.ex?.availableToBack?.[1]?.size,
+              pI: 1,
+              type: "back",
+            },
+            {
+              id: 3,
+              className: "back",
+              value: data?.runners?.[1]?.ex?.availableToBack?.[0]?.price,
+              volume: data?.runners?.[1]?.ex?.availableToBack?.[0]?.size,
+              pI: 0,
+              type: "back",
+            },
+            {
+              id: 4,
+              className: "lay",
+              value: data?.runners?.[1]?.ex?.availableToLay?.[0]?.price,
+              volume: data?.runners?.[1]?.ex?.availableToLay?.[0]?.size,
+              pI: 0,
+              type: "lay",
+            },
+            {
+              id: 5,
+              className: "lay1",
+              value: data?.runners?.[1]?.ex?.availableToLay?.[1]?.price,
+              volume: data?.runners?.[1]?.ex?.availableToLay?.[1]?.size,
+              pI: 1,
+              type: "lay",
+            },
+            {
+              id: 6,
+              className: "lay2",
+              value: data?.runners?.[1]?.ex?.availableToLay?.[2]?.price,
+              volume: data?.runners?.[1]?.ex?.availableToLay?.[2]?.size,
+              pI: 2,
+              type: "lay",
+            },
           ]}
         />
-        <MarketRow
-          //   key={index}
-          title={data?.name}
-          odds={[
-            { className: "back2", value: data.backTeamA, volume: "N/A" },
-            { className: "back1", value: data.backTeamB, volume: "N/A" },
-            { className: "back", value: data.backTeamC, volume: "N/A" },
-            { className: "lay", value: data.layTeamA, volume: "N/A" },
-            { className: "lay1", value: data.layTeamB, volume: "N/A" },
-            { className: "lay2", value: data.layTeamC, volume: "N/A" },
-          ]}
-        />
-        {/* ))} */}
       </div>
     </div>
   );
