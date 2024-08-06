f8060f91d8b299092ecdd5c71644ad697f3f3997
PardeepDuple <pradeep.duple@gmail.com>
2024-08-06 18:17:42 +0530

    fixed header on bollywood casino game mobile view

--- src/components/bollywoodTable/mobile/index.tsx
+++ src/components/bollywoodTable/mobile/index.tsx
@@ -18,8 +18,10 @@ import "./style.scss";
 import MobilePlacedBet from "../../commonComponent/placebet/mobile/myBet";
 import { LoaderOnRefresh } from "../../commonComponent/loader";
 import TiePairBox2 from "./TiePairBox2";
+import MobileMyBet from "../../commonComponent/mybet/mobile/myBet";
 
 const BollywoodTableDesktop = () => {
+  const [activeTab, setActiveTab] = useState(false);
   const [show, setShow] = useState(false);
   const placeBetRef = useRef<HTMLDivElement>(null);
   const [_, setIsSticky] = useState(false);
@@ -28,6 +30,7 @@ const BollywoodTableDesktop = () => {
   const [videoFrameId, setVideoFrameId] = useState(
     `${cardUrl}${cardGamesId.btable}`
   );
+  const { placedBets } = useSelector((state: RootState) => state.bets);
 
   const { dragonTigerDetail, loading } = useSelector(
     (state: RootState) => state.card
@@ -83,136 +86,189 @@ const BollywoodTableDesktop = () => {
 
   return (
     <>
-      <MobilePlacedBet show={show1} setShow={setShow1} />
-      <Row>
-        <Col md={8}>
-          <div className="horseRacingTab">
-            <div style={{ width: "100%", margin: "" }}>
-              <div className="horseRacingTabHeader">
-                <div>
-                  <span style={{ fontSize: "16px", fontWeight: "600" }}>
-                    {dragonTigerDetail?.name}
+      <div className="dt20header">
+        <div className="dt20subheader1">
+          <MobilePlacedBet show={show1} setShow={setShow1} />
+          <div
+            style={{
+              height: "100%",
+              borderTop: !activeTab ? "2px solid white" : "none",
+              padding: "5px",
+            }}
+          >
+            <span
+              style={{ fontSize: "12px", fontWeight: "bold" }}
+              onClick={() => setActiveTab(false)}
+            >
+              GAME
+            </span>
+          </div>
+          <span style={{ fontSize: "18px" }}> | </span>
+          <div
+            style={{
+              height: "100%",
+              borderTop: activeTab ? "2px solid white" : "none",
+              padding: "5px",
+            }}
+          >
+            <span
+              style={{ fontSize: "12px", fontWeight: "bold" }}
+              onClick={() => setActiveTab(true)}
+            >
+              PLACED BET({placedBets?.length || 0})
+            </span>
+          </div>
+        </div>
+        <div className="dt20subheader2">
+          <span
+            style={{ textDecoration: "underline" }}
+            onClick={() => setShow(true)}
+          >
+            Rules
+          </span>
+          <span>
+            {" "}
+            {dragonTigerDetail?.videoInfo
+              ? `Round ID:  ${handleRoundId(dragonTigerDetail?.videoInfo?.mid)}`
+              : ""}{" "}
+          </span>
+        </div>
+      </div>
+      {!activeTab ? (
+        <Row>
+          <Col md={8}>
+            <div className="horseRacingTab">
+              <div style={{ width: "100%", margin: "" }}>
+                <div className="horseRacingTabHeader">
+                  <div>
+                    <span style={{ fontSize: "16px", fontWeight: "600" }}>
+                      {dragonTigerDetail?.name}
+                    </span>
+                    <a
+                      style={{
+                        fontSize: "14px",
+                        textDecoration: "underline",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => setShow(true)}
+                    >
+                      {" "}
+                      RULES
+                    </a>
+                  </div>
+                  <span>
+                    {dragonTigerDetail?.videoInfo
+                      ? `Round ID:  ${handleRoundId(
+                          dragonTigerDetail?.videoInfo?.mid
+                        )}`
+                      : ""}
                   </span>
-                  <a
-                    style={{
-                      fontSize: "14px",
-                      textDecoration: "underline",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => setShow(true)}
-                  >
-                    {" "}
-                    RULES
-                  </a>
                 </div>
-                <span>
-                  {dragonTigerDetail?.videoInfo
-                    ? `Round ID:  ${handleRoundId(
-                        dragonTigerDetail?.videoInfo?.mid
-                      )}`
-                    : ""}
-                </span>
-              </div>
-              <div
-                style={{
-                  width: "100%",
-                  height: "92%",
-                  backgroundColor: "#000",
-                }}
-              >
-                <VideoFrame
-                  time={dragonTigerDetail?.videoInfo?.autotime}
-                  result={<Lucky7Result data={dragonTigerDetail?.videoInfo} />}
-                  id={videoFrameId}
-                />
-              </div>
-            </div>
-            {loading ? (
-              <LoaderOnRefresh />
-            ) : (
-              <div>
-                <div style={{ width: "100%", margin: "" }}>
-                  <TiePairBox
-                    lowHigh={dragonTigerDetail?.players}
-                    data={dragonTigerDetail}
+                <div
+                  style={{
+                    width: "100%",
+                    height: "92%",
+                    backgroundColor: "#000",
+                  }}
+                >
+                  <VideoFrame
+                    time={dragonTigerDetail?.videoInfo?.autotime}
+                    result={
+                      <Lucky7Result data={dragonTigerDetail?.videoInfo} />
+                    }
+                    id={videoFrameId}
                   />
+                </div>
+              </div>
+              {loading ? (
+                <LoaderOnRefresh />
+              ) : (
+                <div>
+                  <div style={{ width: "100%", margin: "" }}>
+                    <TiePairBox
+                      lowHigh={dragonTigerDetail?.players}
+                      data={dragonTigerDetail}
+                    />
+                    <div
+                      style={{
+                        width: "100%",
+                        marginTop: "20px",
+                        borderTop: "1px solid #aaa",
+                      }}
+                    >
+                      <TiePairBox2
+                        lowHigh={[dragonTigerDetail?.luckOdds]}
+                        data={dragonTigerDetail}
+                      />
+                    </div>
+                  </div>
+
                   <div
                     style={{
                       width: "100%",
-                      marginTop: "20px",
-                      borderTop: "1px solid #aaa",
+                      background: "#EEEEEE",
+                      paddingLeft: "4px",
+                      paddingRight: "4px",
+                      marginTop: "10px",
                     }}
                   >
-                    <TiePairBox2
-                      lowHigh={[dragonTigerDetail?.luckOdds]}
+                    <OddEven
+                      name={"DRAGON"}
+                      odds={dragonTigerDetail?.seven}
+                      data={dragonTigerDetail}
+                      card={true}
+                    />
+                  </div>
+
+                  <div
+                    style={{
+                      width: "100%",
+                      background: "#EEEEEE",
+                      paddingLeft: "4px",
+                      paddingRight: "4px",
+                      marginTop: "10px",
+                    }}
+                  >
+                    <OddEven
+                      name={"TIGER"}
+                      odds={dragonTigerDetail?.redBlack}
+                      card={false}
                       data={dragonTigerDetail}
                     />
                   </div>
-                </div>
 
-                <div
-                  style={{
-                    width: "100%",
-                    background: "#EEEEEE",
-                    paddingLeft: "4px",
-                    paddingRight: "4px",
-                    marginTop: "10px",
-                  }}
-                >
-                  <OddEven
-                    name={"DRAGON"}
-                    odds={dragonTigerDetail?.seven}
-                    data={dragonTigerDetail}
-                    card={true}
-                  />
-                </div>
+                  <div
+                    style={{
+                      width: "100%",
+                      background: "#EEEEEE",
+                      paddingLeft: "4px",
+                      paddingRight: "4px",
+                      marginTop: "10px",
+                    }}
+                  >
+                    <CardBox
+                      cardData={dragonTigerDetail?.luckyCards}
+                      data={dragonTigerDetail}
+                      rate={dragonTigerDetail?.luckyCards?.rate}
+                    />
+                  </div>
 
-                <div
-                  style={{
-                    width: "100%",
-                    background: "#EEEEEE",
-                    paddingLeft: "4px",
-                    paddingRight: "4px",
-                    marginTop: "10px",
-                  }}
-                >
-                  <OddEven
-                    name={"TIGER"}
-                    odds={dragonTigerDetail?.redBlack}
-                    card={false}
-                    data={dragonTigerDetail}
-                  />
+                  <div style={{ width: "100%", margin: "" }}>
+                    <CardResultBox
+                      data={dragonTigerDetail}
+                      name={["A", "B", "C", "D", "E", "F"]}
+                      type={cardGamesType.btable}
+                    />
+                  </div>
                 </div>
-
-                <div
-                  style={{
-                    width: "100%",
-                    background: "#EEEEEE",
-                    paddingLeft: "4px",
-                    paddingRight: "4px",
-                    marginTop: "10px",
-                  }}
-                >
-                  <CardBox
-                    cardData={dragonTigerDetail?.luckyCards}
-                    data={dragonTigerDetail}
-                    rate={dragonTigerDetail?.luckyCards?.rate}
-                  />
-                </div>
-
-                <div style={{ width: "100%", margin: "" }}>
-                  <CardResultBox
-                    data={dragonTigerDetail}
-                    name={["A", "B", "C", "D", "E", "F"]}
-                    type={cardGamesType.btable}
-                  />
-                </div>
-              </div>
-            )}
-            <RulesModal show={show} setShow={setShow} rule={luckyrules} />
-          </div>
-        </Col>
-      </Row>
+              )}
+            </div>
+          </Col>
+        </Row>
+      ) : (
+        <MobileMyBet />
+      )}
+      <RulesModal show={show} setShow={setShow} rule={luckyrules} />
       <InactivityModal show={showInactivityModal} handleClose={handleClose} />
     </>
   );
