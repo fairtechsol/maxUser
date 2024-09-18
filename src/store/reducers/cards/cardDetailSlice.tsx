391699e7a83b82b0d682fe0251dd46b87d25fa17
AkashDuple <160487146+AkashDuple@users.noreply.github.com>
2024-09-18 13:55:06 +0530

    NFG-2958
    git commit -m NFG-2958
    git commit -m NFG-2958

--- src/store/reducers/cards/cardDetailSlice.tsx
+++ src/store/reducers/cards/cardDetailSlice.tsx
@@ -667,6 +667,7 @@ const cardDetail = createSlice({
       })
 
       .addCase(updateWorliMatkaRates.fulfilled, (state, action) => {
+        console.log("action.payload",action.payload)
         const { t1, t2 } = action.payload;
         state.loading = false;
         const videoInfo = { ...t1[0] };
