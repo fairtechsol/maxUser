755a5872ba58eae746f7a354dab646721229173b
AkashDuple <160487146+AkashDuple@users.noreply.github.com>
2024-09-19 18:03:41 +0530

    instant-worli

--- src/components/worli/mobile/CardsBox/index.tsx
+++ src/components/worli/mobile/CardsBox/index.tsx
@@ -39,9 +39,10 @@ const CardBox = ({ title, data, cards, odds }: any) => {
   return (
     <>
       <div
-        className={ 
-          data?.worli?.gstatus == 0 ? "suspended abjcardContainer" : " abjcardContainer"
-        }
+        // className={ 
+        //   data?.worli?.gstatus == 0 ? "suspended abjcardContainer" : " abjcardContainer"
+        // }
+        className="abjcardContainer"
         style={{ backgroundColor: "#72bbef", border: "0.5px solid #9e9e9e" }}
       >
         <div
