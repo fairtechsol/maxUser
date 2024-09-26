7493e449b0eb0aaeafab227e0e5ba4c3928da492
PardeepDuple <pradeep.duple@gmail.com>
2024-09-26 11:59:49 +0530

    revert for live branch

--- src/utils/constants/index.tsx
+++ src/utils/constants/index.tsx
@@ -438,10 +438,10 @@ export const navigateToGameDetail = {
 
 export const cardUrl =
   process.env.NODE_ENV === "production"
-    ? "https://maxbet07.com/videoPage/"
+    ? "https://jmdapi.com/tablevideo/?id="
     : "https://maxbet07.com/videoPage/";
 
-export const cardUrlMain =  "https://maxbet07.com/videoPage/";
+export const cardUrlMain = "https://jmdapi.com/tablevideo/?id=";
 export const scoreBoardUrlMain = "https://score.hr08bets.in/api?eventId=";
 
 // export const scoreBoardUrlMain =
