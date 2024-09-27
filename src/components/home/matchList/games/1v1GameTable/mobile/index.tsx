0024b56071160dbddcafdfe986a11961d6b8f6d1
kamal <154864041+kamal9780@users.noreply.github.com>
2024-09-27 16:00:24 +0530

    added tvIcon for mobile in matchListpage

--- src/components/home/matchList/games/1v1GameTable/mobile/index.tsx
+++ src/components/home/matchList/games/1v1GameTable/mobile/index.tsx
@@ -14,6 +14,7 @@ import ContactAdmin from "../../../../../commonComponent/contactAdmin";
 import HorseRacingComponentList from "../../../../../horseRacing";
 import BackLayComponent from "./backlayComponent";
 import "./style.scss";
+import { TbDeviceTvOld } from "react-icons/tb";
 
 const MobileOneVOneGame = ({ mTypeid }: any) => {
   const { matchList } = useSelector(
@@ -173,6 +174,7 @@ const MobileOneVOneGame = ({ mTypeid }: any) => {
                                     ) : (
                                       ""
                                     )}
+                                { item?.isTv === true || item?.isTv === "1" ? <TbDeviceTvOld /> : "" }
                                     {item?.manualSessionActive ||
                                     item?.apiSessionActive ? (
                                       <span className="fancy">
