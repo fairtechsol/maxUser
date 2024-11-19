b6f58cb793baca312dbb21a2dd4224f48359f5f6
kamal <154864041+kamal9780@users.noreply.github.com>
2024-11-19 19:56:44 +0530

    LIVEcasino Games for Mobile

--- src/pages/casinoSubTab/index.tsx
+++ src/pages/casinoSubTab/index.tsx
@@ -1,119 +1,33 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
+import { AppDispatch, RootState } from "../../store/store";
+import { useDispatch } from "react-redux";
+import { liveCasinoList } from "../../store/actions/cards/cardDetail";
+import { isMobile } from "../../utils/screenDimension";
+import LiveCasinoMobile from "../../components/liveCasino/mobile";
+import LiveCasinoDesktop from "../../components/liveCasino/desktop";
 
 const LiveCasino = () => {
-  const { liveCasinoData } = useSelector((state: RootState) => state.card);
-  const initialType =
-    liveCasinoData && Object.keys(liveCasinoData).length > 0
-      ? Object.keys(liveCasinoData)[0]
-      : null;
+  const dispatch: AppDispatch = useDispatch();
 
-  const [list, setList] = useState<Record<string, any>>({});
-  const [type, setType] = useState<string>("");
-  const [type2, setType2] = useState<string>("");
-  const [game, setGame] = useState<any>();
-  const [isLoading, setIsLoading] = useState(true);
+  const { loading } = useSelector((state: RootState) => state.card);
+
 
   useEffect(() => {
-    if (liveCasinoData && Object.keys(liveCasinoData).length > 0) {
-      setList(liveCasinoData);
-      setType(Object.keys(liveCasinoData)[0]);
-      const firstKey = Object.keys(liveCasinoData[initialType])[0];
-      setType2(Object.keys(liveCasinoData[initialType])[0]);
-      const firstObject = liveCasinoData[initialType][firstKey];
-      setGame(firstObject);
-      setIsLoading(false);
-    }
-  }, [liveCasinoData]);
+    dispatch(liveCasinoList("asbd"));
+  },[]);
 
-  if (isLoading) {
+
+  if (loading) {
     return (
       <div className="w-100 d-flex justify-content-center align-items-center">
         <p>Loading...</p>
       </div>
     );
   }
-  const LiveCasinoTab = ({ data2 }: { data2: any }) => {
-    return (
-      <div className="w-100 d-flex flex-row bg-tab">
-        {Object.keys(data2)?.map((item: any, index: number) => {
-          const isActive = item === type2 ? true : false;
-          return (
-            <div
-              key={index}
-              onClick={() => {
-                setGame(data2[item]);
-                setType2(item);
-              }}
-              className="d-flex justify-content-center align-items-center py-1 px-4"
-              style={{
-                cursor: "pointer",
-                backgroundColor: isActive ? "#004A25" : "",
-                color: isActive ? "#fff" : "#000",
-              }}
-            >
-              {item}
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
-  const LiveCasinoGames = ({ data3 }: { data3: any }) => {
-    return (
-      <div
-        className="w-100 d-flex flex-row flex-wrap mt-1"
-        style={{ gap: "10px" }}
-      >
-        {data3?.map((item: any, index: number) => {
-          return (
-            <img
-              key={index}
-              src={item?.url_thumb}
-              className="img-fluid"
-              alt={item?.game_name}
-              loading="lazy"
-              style={{ width: "19%", height: "10vh" }}
-            />
-          );
-        })}
-      </div>
-    );
-  };
-  const handleParent = (key: any) => {
-    setType(key);
-    const firstKey = Object.keys(liveCasinoData[key])[0];
-    setType2(firstKey);
-    setGame(liveCasinoData[key][firstKey]);
-  };
-  return (
-    <div className="w-100 d-flex flex-row mt-1 gap-2">
-      <div className="w-25 h-100 d-flex flex-column bg-secondary">
-        {Object.keys(list)?.map((key, index) => {
-          const isActive = type === key;
-          return (
-            <div
-              key={index}
-              onClick={() => handleParent(key)}
-              className={`w-100 d-flex justify-content-center align-items-center py-2 ${
-                isActive ? "bg-tab text-white" : ""
-              }`}
-              style={{
-                cursor: "pointer",
-              }}
-            >
-              {key}
-            </div>
-          );
-        })}
-      </div>
 
-      <div className="w-75 d-flex flex-column">
-        <LiveCasinoTab data2={list[type]} />
-        <LiveCasinoGames data3={game ?? []} />
-      </div>
-    </div>
+  return (
+    isMobile ? <LiveCasinoMobile/> : <LiveCasinoDesktop/> 
   );
 };
 
