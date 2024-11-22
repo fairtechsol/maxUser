5f98bec199068a8e30658dc227441d189d738929
PardeepDuple <pradeep.duple@gmail.com>
2024-11-22 12:19:18 +0530

    changes to remove history and added toast error for code  429

--- src/pages/auth/login/login.tsx
+++ src/pages/auth/login/login.tsx
@@ -56,7 +56,7 @@ const Login = () => {
         navigate("/change-password");
       } else {
         dispatch(rulesModalShowTrue());
-        navigate("/home");
+        navigate("/home", { replace: true });
       }
       dispatch(authReset());
     }
