c420bf7272b7ffbc61054dca6e3750811a27bb3a
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-11-22 17:44:34 +0530

    fix

--- src/utils/constants/index.tsx
+++ src/utils/constants/index.tsx
@@ -149,7 +149,7 @@ export const Constants = {
   thirdPartyLive: "https://serviceapi.fairgame7.com",
   expertPathLive: "https://expertapi.fairgame7.com",
   thirdPartyCardLive: "https://casinoserviceapi.fairgame7.com",
-  localPath: "https://devmaxbet9api.fairgame.club",
+  localPath: "http://localhost:5000",
   localPathExpert: "http://localhost:6060",
 };
 
