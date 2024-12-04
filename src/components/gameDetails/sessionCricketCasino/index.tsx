fca64b300603c01591d34fecd627f0ee2cfdbbfa
dhanesh241 <143992210+dhanesh241@users.noreply.github.com>
2024-12-04 12:09:52 +0530

    padding to icon

--- src/components/gameDetails/sessionCricketCasino/index.tsx
+++ src/components/gameDetails/sessionCricketCasino/index.tsx
@@ -97,7 +97,7 @@ const SessionCricketCasino = ({ title, data, detail }) => {
           >
             {title}
           </span>
-          { shouldShowInfoIcon && <OverlayTrigger placement="top" overlay={tooltip}><div><IoInformationCircle size={20}/></div></OverlayTrigger>}
+          { shouldShowInfoIcon && <OverlayTrigger placement="top" overlay={tooltip}><div className="px-2"><IoInformationCircle size={20}/></div></OverlayTrigger>}
         </div>
         <div
           style={{
