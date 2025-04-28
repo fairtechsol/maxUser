1ac27b28a8017ce20581f5c62ea8c79d06eab9d7
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-04-28 16:45:37 +0530

    refactor: enhance layout structure and scrolling behavior in DesktopGameDetail component

--- src/components/gameDetails/desktop/index.tsx
+++ src/components/gameDetails/desktop/index.tsx
@@ -357,27 +357,36 @@ const DesktopGameDetail = () => {
                   : "100%",
               }}
             >
-              {matchDetails?.eventId &&
-                matchDetails?.matchType !== "politics" && (
-                  <Col md={12} className="px-1 pt-1">
-                    <LiveStreamComponent
-                      url={
-                        import.meta.env.VITE_NODE_ENV == "production"
-                          ? tvData?.tvData?.iframeUrl
-                          : `${liveStreamPageUrl}${matchDetails?.eventId}/${matchDetails?.matchType}`
-                      }
-                      eventId={matchDetails?.eventId}
-                      matchType={matchDetails?.matchType}
-                      setTvData={setTvData}
-                    />
-                  </Col>
-                )}
-              <Col md={12} className="px-1 pt-1">
-                <PlacedBet />
-              </Col>
-              <Col md={12} className="px-1 pt-1 ">
-                <MyBet />
-              </Col>
+              <div
+                className="scrollable-content"
+                style={{
+                  maxHeight: "100vh",
+                  overflowY: "auto",
+                  paddingBottom: "20px",
+                }}
+              >
+                {matchDetails?.eventId &&
+                  matchDetails?.matchType !== "politics" && (
+                    <div className="px-1 pt-1">
+                      <LiveStreamComponent
+                        url={
+                          import.meta.env.VITE_NODE_ENV == "production"
+                            ? tvData?.tvData?.iframeUrl
+                            : `${liveStreamPageUrl}${matchDetails?.eventId}/${matchDetails?.matchType}`
+                        }
+                        eventId={matchDetails?.eventId}
+                        matchType={matchDetails?.matchType}
+                        setTvData={setTvData}
+                      />
+                    </div>
+                  )}
+                <div className="px-1 pt-1">
+                  <PlacedBet />
+                </div>
+                <div className="px-1 pt-1">
+                  <MyBet />
+                </div>
+              </div>
             </Row>
           </Container>
         </Col>
